81cb1f0c891a36d85df68fd39bfb5b3673cd4434
Dhanar J Kusuma <dhanar.januardhi.k@mail.ugm.ac.id>
2018-09-26 12:45:19 +0700

    update js link, update bash

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 # Change Directory Bin
 DIR="$( cd "$( dirname $0 )" >/dev/null && pwd )"
-echo "[Portofolio App] Changing directory $DIR--"
+echo "[Library IS Page -  App] Changing directory $DIR--"
 
-echo "[Portofolio App] Checking Bin"
+echo "[Library IS Page -  App] Checking Bin"
 appBin="$DIR/bin"
 if [ -f "$appBin/main" ]
 then
     eval "$appBin/main"
 else
-    echo "[Portofolio App] Trying to Installing Portofolio Application" 
+    echo "[Library IS Page -  App] Trying to Installing Portofolio Application" 
     # Change Directory App/Bin
     cdCmd="cd $appBin/../" 
     eval $cdCmd
